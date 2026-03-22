562deb83861f40162db4e6ddcc9b0bc9f85aaa06
eloyrobillard <eloy.robillard@gmail.com>
2026-03-22 11:22:55 +0100

    Remove private functions from header

--- mesh.h
+++ mesh.h
@@ -22,11 +22,8 @@ typedef struct {
 
 Mesh load(const std::string &filename);
 void draw(Shader::Shader &shader, Mesh &mesh);
-GLuint createVertexArray(const float *verts, uint numVerts, const uint *indices,
-                         uint numIndices, size_t sizeVert);
 void deleteVertexArray(GLuint vertexBuffer, GLuint indexBuffer,
                        GLuint vertexArray);
-void setVerticesActive(GLuint vertexArray);
 } // namespace Mesh
 
 #endif // MESH_H
