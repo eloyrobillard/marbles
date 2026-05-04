f4e0275f281bcf1f027c3732f6017ccdff1a2099
eloyrobillard <eloy.robillard@gmail.com>
2026-05-04 19:12:48 +0200

    Fix OpenGL bad enum value error

--- renderer.cpp
+++ renderer.cpp
@@ -5,7 +5,6 @@
 // SOURCE: https://lackeyccg.com/glfont.c
 void SDL_GL_Enter2DMode() {
   glDisable(GL_DEPTH_TEST);
-  glEnable(GL_TEXTURE_2D);
 
   /* This allows alpha blending of 2D textures with the scene */
   glEnable(GL_BLEND);
@@ -14,7 +13,6 @@ void SDL_GL_Enter2DMode() {
 
 void SDL_GL_Leave2DMode() {
   glEnable(GL_DEPTH_TEST);
-  glDisable(GL_TEXTURE_2D);
 
   /* This allows alpha blending of 2D textures with the scene */
   glDisable(GL_BLEND);
