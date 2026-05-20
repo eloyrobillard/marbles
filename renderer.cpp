7d11440a066264e32e859eec20d61d645ca9e62e
eloyrobillard <eloy.robillard@gmail.com>
2026-05-20 16:45:21 +0200

    Fix bloom leaking at top of screen

--- renderer.cpp
+++ renderer.cpp
@@ -308,8 +308,10 @@ GLuint Renderer::createColorAttachmentTexture(int width, int height,
                GL_UNSIGNED_BYTE, nullptr);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
   glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
   glFramebufferTexture2D(GL_FRAMEBUFFER, colorAttachment, GL_TEXTURE_2D,
-                         texture, 0); // we only need a color buffer
+                         texture, 0);
 
   return texture;
 }
