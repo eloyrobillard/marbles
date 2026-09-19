47611e310e92b7463e7ef3f38cfc4ba601f7381e
eloyrobillard <eloy.robillard@gmail.com>
2026-09-19 17:19:27 +0200

    Fix audio volume not changing in real-time

--- audio.hpp
+++ audio.hpp
@@ -105,9 +105,9 @@ struct AudioMachine {
   static void OnGround(f32 onryo) {
     auto queuedBytes = SDL_GetAudioStreamQueued(rollStream);
 
-    if (queuedBytes < rollLength) {
-      SDL_SetAudioStreamFrequencyRatio(rollStream, log10f(onryo + 1.f));
+    SDL_SetAudioStreamFrequencyRatio(rollStream, log10f(onryo + 1.f));
 
+    if (queuedBytes < rollLength) {
       if (!SDL_PutAudioStreamData(rollStream, rollBuffer, rollLength)) {
         SDL_Log("Error: Failed to put audio in the stream: %s", SDL_GetError());
       }
